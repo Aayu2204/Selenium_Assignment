c3551b0dc1efcb9094f38a2054e6b8f4d04a6b97
Aayu2204 <89973923+Aayu2204@users.noreply.github.com>
2021-09-02 20:14:56 +0530

    Add files via upload

---
+++ MobilesScreen.cs
@@ -0,0 +1,76 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class MobilesScreen
+    {
+        public IWebDriver driver;
+        public MobilesScreen(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        By btnMobileAccessories = By.XPath("//div[contains(@id,'nav-subnav')]//span[contains(text(),'Mobiles & Accessories')]");
+        By btnPowerBank = By.XPath("//a[contains(text(),'Power Banks')]");
+        By btnAccountsAndList = By.XPath("//span[contains(text(),'Account & Lists')]");
+        By btnSignIn = By.XPath("//a/span[contains(text(),'Sign in')]");
+        By txtUsername = By.XPath("//input[contains(@id,'ap_email')]");
+        By txtPasword = By.XPath("//input[contains(@id,'ap_password')]");
+        By btnContinue = By.XPath("//input[contains(@id,'continue')]");
+        By btnLogIn = By.XPath("//input[contains(@id,'signInSubmit')]");
+        By lblChange = By.XPath("//a[contains(text(),'Change')]");
+        public void clickPowerBankOption()
+        {
+            Actions action = new Actions(driver);
+            action.MoveToElement(driver.FindElement(btnMobileAccessories)).Build().Perform();
+            Thread.Sleep(3000);
+            IWebElement powerBank = driver.FindElement(btnPowerBank);
+            powerBank.Click();
+            Thread.Sleep(3000);
+        }
+
+        public void dataDriven_LoginScenario()
+        {
+            string CSV_FilePath = AppDomain.CurrentDomain.BaseDirectory + "Book1.csv";
+            Actions action = new Actions(driver);
+            action.MoveToElement(driver.FindElement(btnAccountsAndList)).Build().Perform();
+            Thread.Sleep(4000);
+            IWebElement clickSignInBtn = driver.FindElement(btnSignIn);
+            clickSignInBtn.Click();
+            Thread.Sleep(4000);
+            using (var reader = new StreamReader(CSV_FilePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+                    IWebElement txtUserName = driver.FindElement(txtUsername);
+                    txtUserName.SendKeys(values[0].ToString());
+                    Thread.Sleep(1000);
+                    IWebElement clickContinueBtn = driver.FindElement(btnContinue);
+                    clickContinueBtn.Click();
+                    Thread.Sleep(4000);
+                    IWebElement txtPassword = driver.FindElement(txtPasword);
+                    txtPassword.SendKeys(values[1].ToString());
+                    Thread.Sleep(5000);
+                    IWebElement btnSubmit = driver.FindElement(btnLogIn);
+                    btnSubmit.Click();
+                    Thread.Sleep(5000);
+                    IWebElement linklblChange = driver.FindElement(lblChange);
+                    linklblChange.Click();
+                    Thread.Sleep(4000);
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Program
+    {
+        static String logtablename;
+        static String Clientname;
+        static string Insurance_Plan;
+
+        static void Main(string[] args)
+        {
+            ChromeDriver driver = null;
+            String url;
+            String userid;
+            String password, Greeting = null; ;
+            String Downloadfolderpath, Downloadfolderpath1;
+            String claim_Files_Id = null, supporting_Files_Id;
+
+
+            Assignment.MobilesScreen dashboardObj = null;
+            try
+            {
+
+                By btnMobilesScreen = By.XPath("//a[contains(text(),'Mobiles')]");
+
+                driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+
+                driver.Url = @"https://www.amazon.in/";
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(40);
+
+                dashboardObj = ClickMobileScreenBtn(driver, btnMobilesScreen);
+                Thread.Sleep(1000);
+
+                Thread.Sleep(1000);
+                dashboardObj.clickPowerBankOption();
+                Thread.Sleep(5000);
+                dashboardObj.dataDriven_LoginScenario();
+                Thread.Sleep(4000);
+
+
+            }
+            catch (Exception e)
+            {
+
+                //ceobj.sendmailDataExtraction(Clientname, Insurance_Plan, e.Message, con, claim_Files_Id);
+
+
+
+            }
+            driver.Quit();
+            Thread.Sleep(2000);
+            Environment.Exit(-1);
+
+        }
+
+
+
+        private static Assignment.MobilesScreen ClickMobileScreenBtn(IWebDriver driver, By btnSubmit)
+        {
+
+            IWebElement buttonElement = driver.FindElement(btnSubmit);
+            if (buttonElement.Displayed || buttonElement.Enabled)
+                try
+                {
+                    buttonElement.Click();
+                }
+                catch (Exception e)
+                {
+
+                }
+            return new Assignment.MobilesScreen(driver);
+        }
+    }
+}
